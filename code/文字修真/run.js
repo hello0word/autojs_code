fc6aa3113f5ae3a6ae87dada6121c7a139c94025
XJJ <1004864951@qq.com>
2020-01-21 10:24:12 +0800

    1111111111

--- code/文字修真/run.js
+++ code/文字修真/run.js
@@ -131,7 +131,7 @@ function 检查一遍() {
     }
     var 第三个 = images.findImageInRegion(IMG, 已完成, 137, 1350, 800, 350)
     if (!第三个) {
-        var 开始坐标 = images.findImageInRegion(IMG, 开始, 137, 1000, 800, 350)
+        var 开始坐标 = images.findImageInRegion(IMG, 开始, 137, 1350, 800, 350)
         if (开始坐标) {
             click(开始坐标.x, 开始坐标.y)
             sleep(60 *1000)
