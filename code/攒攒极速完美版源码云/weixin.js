87a9ef52725b17d1574cec895b538cf5b3399aae
XJJ <1004864951@qq.com>
2020-03-06 19:33:39 +0800

    111111111

--- code/攒攒极速完美版源码云/weixin.js
+++ code/攒攒极速完美版源码云/weixin.js
@@ -10,7 +10,7 @@ function main(){
     while (true) {
         let scr = scrollable().find()
         scr.scrollForward()
-        sleep(4000)
+        sleep(2000)
     }
 }
 function work(){
@@ -18,6 +18,7 @@ function work(){
     while (true) {
         if (text("开始阅读").exists()) {
             jishu +=1
+            toast("开始阅读:"+jishu)
             if (jishu >7) {
                 text("开始阅读").findOne().click()
                 log("开始阅读")
@@ -30,10 +31,19 @@ function work(){
 
 function work2(){
     while(true){
-        if(descContains("在看").exists() || desc("阅读原文").exists() ){
-            log("在看")
-            sleep(10000)
-            back()
+        if(descContains("在看").exists()  ){
+            if (descContains("再看").findOne().bounds().centerY() > device.height * 0.82 ) {
+                toast("10秒后返回")
+                sleep(10000)
+                back()
+            }
+            
+        } else if (desc("阅读原文").exists()){
+            if (descContains("阅读原文").findOne().bounds().centerY() > device.height * 0.82) {
+                toast("10秒后返回")
+                sleep(10000)
+                back()
+            }
         }
         sleep(3000)
     }
