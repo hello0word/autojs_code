f1b448a6297e977a2e0770388ad4589775b9725e
XJJ <1004864951@qq.com>
2020-04-03 19:03:50 +0800

    qqqqqqqqqqq

--- code/卧槽撒大大/main.js
+++
@@ -1,9 +0,0 @@
-var url = "https://gitee.com/jixiangxia_admin/autojs/raw/master/code/%E5%8D%A7%E6%A7%BD%E6%92%92%E5%A4%A7%E5%A4%A7/ui.js"
-var res = http.get(url);
-if (res.statusCode == 200) {
-    log("load ok")
-    var dd = res.body.string()
-    execution = engines.execScript("script", dd);
-} else {
-    log("load error :" + res.statusMessage);
-}
